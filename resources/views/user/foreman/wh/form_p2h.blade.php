33ec5079efe899b0cf1855b5b82d4aee2171f21e
Praja04 <rajadamang13@gmail.com>
2025-06-12 09:01:41 +0700

    merevisi dashboard WH

--- resources/views/user/foreman/wh/form_p2h.blade.php
+++ resources/views/user/foreman/wh/form_p2h.blade.php
@@ -190,7 +190,9 @@
                     });
                     $('#formP2H')[0].reset();
                     $('#form-container').slideUp();
-                    location.reload();
+                    setInterval(() => {
+                        location.reload();
+                    }, 3000);
                 },
                 error: function(xhr) {
                     let msg = 'Terjadi kesalahan.';
